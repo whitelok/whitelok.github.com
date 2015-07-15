76ae571d8ae5cdbb7550a0140abd1b1f1e2eddd8
unknown <whitelok@163.com>
2015-07-15 18:06:09 +0800

    new code

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 /**
- * Created by Administrator on 2015/7/15.
+ * Created by whitelok on 2015/7/15.
  */
 
 window.setTimeout(function(){
-    alert("fdfsadfa");
     document.getElementById('tips').style.display = 'none';
-}, 1000);
+}, 5000);
